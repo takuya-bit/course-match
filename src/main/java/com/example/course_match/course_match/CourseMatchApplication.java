6796c531b25521e67146535794b68008cf942d15
takuya shimura <takuyashimura@takuyanoMacBook-Air.local>
2024-11-24 00:55:29 +0900

    first commit

---
+++ src/main/java/com/example/course_match/course_match/CourseMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_match.course_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_match/course_match/CourseMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_match.course_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
